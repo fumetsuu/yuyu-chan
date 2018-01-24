c33bfc01d19867783f833e0894a8f3cb29369fe2
Joseph Surin <joseph.surin@gmail.com>
2018-01-24 08:09:32 +0800

    add removesticker command

--- src/commands.js
+++ src/commands.js
@@ -1,9 +1,11 @@
 const sticker = require('./commands/sticker.js')
 const addsticker = require('./commands/addsticker.js')
+const removesticker = require('./commands/removesticker.js')
 
 const commands = {
     "sticker": sticker,
-    "addsticker": addsticker
+    "addsticker": addsticker,
+    "removesticker": removesticker
 }
 
 module.exports = commands
