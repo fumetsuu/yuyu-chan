6cfc329fc5bfac555d481249bd530abdd8734590
Joseph Surin <joseph.surin@gmail.com>
2018-01-28 11:02:09 +0800

    loading msg for sticker preview

--- src/commands/stickerspreview.js
+++ src/commands/stickerspreview.js
@@ -5,22 +5,53 @@ const previewDataPath = './src/stickers/previewData.json'
 
 module.exports = function stickerspreview(msg, args) {
 	logger.info('called', 'trying to call genStickersPreview()')
+	var forceUpdate = false
+	if (args.includes('force=true')) {
+		forceUpdate = true
+	}
 	//store number of stickers in previewInfo.json then check with stickers folder if there is a differing number of stickers
 	var stickersInFolder = fs.readdirSync('./src/stickers/stickerImgs').length
 	fs.readFile(previewDataPath, (err, data) => {
 		var previewData = JSON.parse(data)
-		if (!previewData.stickerCount || previewData.stickerCount != stickersInFolder) {
-			utils.genStickersPreview('./src/stickers/preview.jpg', async (err, file, count) => {
+		if (!previewData.stickerCount || previewData.stickerCount != stickersInFolder || forceUpdate) {
+			var loadingMsg
+			msg.channel
+				.send({
+					embed: {
+						title: 'generating preview image',
+						description: 'please wait...'
+					}
+				})
+				.then(message => {
+					loadingMsg = message
+				})
+			utils.genStickersPreview('./src/stickers/preview.jpg', (err, file, count) => {
 				previewData['stickerCount'] = count //update json file
 				previewData['previewFile'] = file
 				logger.success('sending', 'sending new preview')
-				await msg.channel.send({ files: [file] })
-				fs.writeFile(previewDataPath, JSON.stringify(previewData))
+				msg.channel.send({ files: [file] }).then(msg => {
+					loadingMsg.delete()
+					logger.info('sent', 'sent new preview')
+					fs.writeFileSync(previewDataPath, JSON.stringify(previewData))
+				})
 			})
 		} else if (previewData.stickerCount == stickersInFolder) {
+			var loadingMsg
+			msg.channel
+				.send({
+					embed: {
+						title: 'sending preview image',
+						description: 'please wait...'
+					}
+				})
+				.then(message => {
+					loadingMsg = message
+				})
 			//if stickerCount exists and is the same as number of files
 			logger.success('sending', 'sending unchanged preview!!')
-			msg.channel.send({ files: [previewData.previewFile] })
+			msg.channel.send({ files: [previewData.previewFile] }).then(msg => {
+				loadingMsg.delete()
+			})
 		} else {
 			logger.err('huh', 'uhmmmmm')
 		}
